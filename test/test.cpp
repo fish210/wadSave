d8a4f8b6c38f383427d703b54e2d3b4e1d3d9587
fish210 <82754316+fish210@users.noreply.github.com>
2021-04-27 01:42:01 +0800

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -10,11 +10,11 @@ int main()
 {
     string directory = "directory";
     fileSearch(directory, directory.length() + 1, "", &wad_struct);//历遍文件, 包括子目录
-	write_EntryData("new.wad.client", 1);//写条目数据, 0表示不压缩 1表示zstd压缩
-	write_EntryInfo("new.wad.client", 1);//写条目信息, 0表示不压缩 1表示zstd压缩
+    write_EntryData("new.wad.client", 1);//写条目数据, 0表示不压缩 1表示zstd压缩
+    write_EntryInfo("new.wad.client", 1);//写条目信息, 0表示不压缩 1表示zstd压缩
 
-	int a;
-	cin >> a;
+    int a;
+    cin >> a;
 }
 
 
